318523e65721f9afeb5c0bce8017f14912900600
Mattriks <Mattriks@users.noreply.github.com>
2017-04-24 15:38:07 +1000

    Julia 0.6 update

--- src/MLKernels.jl
+++ src/MLKernels.jl
@@ -2,9 +2,14 @@
 #==========================================================================
  Functions from Julia package MLKernels.jl
  Unfortunately MLKernels hasn't tagged a new release for a while
- Hence I've borrowed (and modified) some functions from an earlier vesion
+ Hence I've borrowed (and modified) some functions from vesion 0.1.0
  of MLKernels here, see
-  https://github.com/trthatcher/MLKernels.jl/blob/master/LICENSE.md
+  https://github.com/trthatcher/MLKernels.jl/blob/master/LICENSE.md:
+"The MIT License (MIT)
+Copyright (c) 2015 Tim Thatcher
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
 ==========================================================================#
 
 export Kernel
